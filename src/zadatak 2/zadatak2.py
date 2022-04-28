4b249f98502888cfdcefc298f60983379b6630aa
Dusan Brkic <dusanbrk@gmail.com>
2022-04-28 19:54:44 +0200

    dodat kernel

--- src/zadatak 2/zadatak2.py
+++ src/zadatak 2/zadatak2.py
@@ -1,7 +1,55 @@
 import sys
 import numpy as np
 import pandas
-from matplotlib import pyplot as plt
+
+
+class SquaredExponentialKernel:
+    def __init__(self, sigma_f: float = 1, length: float = 1):
+        self.sigma_f = sigma_f
+        self.length = length
+
+    def __call__(self, argument_1: np.array, argument_2: np.array) -> float:
+        return float(self.sigma_f *
+                     np.exp(-(np.linalg.norm(argument_1 - argument_2)**2) /
+                            (2 * self.length**2)))
+
+
+class KRRS:
+    def __init__(self, trainData, kernelFunc, lambdaPara):
+        self.trainX = trainData[0]
+        trainY = trainData[1]
+
+        self.kernelFunc = kernelFunc
+
+        kArr = np.empty(
+            (self.trainX.shape[0], self.trainX.shape[0]), dtype=np.float64)  # zeros
+
+        for i in range(0, self.trainX.shape[0]):
+            for j in range(0, self.trainX.shape[0]):
+                xi = self.trainX[i]
+                xj = self.trainX[j]
+
+                kij = kernelFunc(xi, xj)
+
+                kArr[i][j] = kij
+
+        ridgeParas = lambdaPara * \
+            np.identity(self.trainX.shape[0], dtype=np.float64)
+
+        # alpha for kernel ridge $\alpha = (\Phi(X)\phi^T(X)+\lambda I)^{-1}Y$
+        self.alpha = np.dot(np.linalg.inv(np.add(kArr, ridgeParas)), trainY)
+
+    def predict(self, testX):
+        YPred = np.empty((testX.shape[0]), dtype=np.float64)  # zeros
+        for testInd in range(0, testX.shape[0]):
+
+            xnew = testX[testInd]
+            # for i in range(0, trainX.shape[0]):   # $y_{new} = \sum_{i}  \alpha_i \Phi(x_i) \Phi(x_{new})
+            # innerVal =
+            YPred[testInd] = np.sum([np.dot(self.alpha[i], self.kernelFunc(
+                self.trainX[i], xnew)) for i in range(0, self.trainX.shape[0])])  # sum ??
+
+        return YPred
 
 
 class Ridge:
@@ -9,15 +57,16 @@ class Ridge:
         self.alfa = alfa
         self.koeficijenti = None
 
-    def treniraj(self, X, y):
+    def fit(self, X, y):
         dim = X.shape
         jedinicna = np.identity(dim[1])
 
         self.koeficijenti = np.dot(np.dot(np.linalg.inv(
             np.dot(X.T, X) + self.alfa * jedinicna), X.T), y)
-        return
 
-    def predvidi(self, parametri):
+        return self.koeficijenti
+
+    def predict(self, parametri):
         # print(self.koeficijenti)
         y = [0 for o in range(len(parametri))]
         for j in range(len(parametri)):
@@ -76,28 +125,30 @@ def main():
     plate_test = test_data["plata"]
     test_data.drop(["plata"], inplace=True, axis=1)
 
-    train_X = train_data
-    train_y = plate
+    train_X = train_data.to_numpy()
+    train_y = plate.to_numpy()
     test_X = test_data.to_numpy()
     test_y = plate_test.to_numpy()
 
+    train_X = stepenuj_matricu(train_X, 1)
+    train_X = dodaj_jedinice(train_X)
     test_X = stepenuj_matricu(test_X, 1)
     test_X = dodaj_jedinice(test_X)
 
-    np.set_printoptions(threshold=np.inf)
-    regression = Ridge(0.001389)
-    train_X = stepenuj_matricu(train_X.to_numpy(), 1)
-    train_X = dodaj_jedinice(train_X)
-    regression.treniraj(train_X, train_y)
+    # regression = Ridge(0.001389)
+    # regression.fit(train_X, train_y)
+    # pred_Y = regression.predict(test_X)
 
-    # print(test_X)
+    # print(rmse(test_y, pred_Y))
 
-    pred_Y = regression.predvidi(test_X)
+    model = KRRS([train_X, train_y],
+                 SquaredExponentialKernel(5, 10),
+                 0.01
+                 )
+    pred_Y = model.predict(test_X)
 
     print(rmse(test_y, pred_Y))
 
-    # print(train_X)
-    # print(train_y)
     return
 
 
