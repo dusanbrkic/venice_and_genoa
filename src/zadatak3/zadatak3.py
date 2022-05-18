73f77377b39b3d158742ee2375f1110f79a434a1
Dusan Brkic <dusanbrk@gmail.com>
2022-05-18 05:46:07 +0200

    Update zadatak3.py

--- src/zadatak3/zadatak3.py
+++ src/zadatak3/zadatak3.py
@@ -1,3 +1,4 @@
+import sys
 import pandas as pd
 import string
 from sklearn.feature_extraction.text import CountVectorizer
@@ -5,7 +6,7 @@ from sklearn.feature_extraction import text
 from sklearn import svm
 from sklearn.metrics import classification_report
 from sklearn.metrics import f1_score
-
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def skloni_interpunkciju(recenica):
     for ch in string.punctuation:
@@ -21,11 +22,11 @@ def skloni_stopwords(recenica):
 
 def main():
     # print(sys.argv)
-    #train_file_path = sys.argv[1]
-    #test_file_path = sys.argv[2]
+    train_file_path = sys.argv[1]
+    test_file_path = sys.argv[2]
 
-    train_file_path = "res/train.json"
-    test_file_path = "res/test.json"
+    # train_file_path = "res/train.json"
+    # test_file_path = "res/test.json"
 
     train_data = pd.read_json(train_file_path)
     test_data = pd.read_json(test_file_path)
@@ -33,14 +34,14 @@ def main():
     train_data['text'] = train_data['text'].apply(
         lambda x: skloni_interpunkciju(x))
     train_data['text'] = train_data['text'].apply(lambda x: x.lower())
-    train_data['text'] = train_data['text'].apply(
-        lambda x: skloni_stopwords(x))
+    # train_data['text'] = train_data['text'].apply(
+    #     lambda x: skloni_stopwords(x))
 
     test_data['text'] = test_data['text'].apply(
         lambda x: skloni_interpunkciju(x))
     test_data['text'] = test_data['text'].apply(lambda x: x.lower())
-    test_data['text'] = test_data['text'].apply(
-        lambda x: skloni_stopwords(x))
+    # test_data['text'] = test_data['text'].apply(
+    #     lambda x: skloni_stopwords(x))
 
     X_train = train_data['text']
     Y_train = train_data['clickbait']
@@ -48,18 +49,19 @@ def main():
     X_test = test_data['text']
     Y_test = test_data['clickbait']
 
-    vec = CountVectorizer()
+    # vec = CountVectorizer()
+    vec = TfidfVectorizer(ngram_range=(1,2), sublinear_tf=True)
     # print(X_train)
     X_train = vec.fit_transform(X_train)
     X_test = vec.transform(X_test)
     # print(vec)
 
-    klasifikator = svm.SVC(kernel='rbf', gamma=0.15)
+    klasifikator = svm.SVC(kernel='sigmoid', coef0=0.1, gamma=1)
     klasifikator.fit(X_train, Y_train)
 
     y_predicted = klasifikator.predict(X_test)
 
-    print(classification_report(Y_test, y_predicted))
+    # print(classification_report(Y_test, y_predicted))
     print(f1_score(Y_test, y_predicted))
 
 
